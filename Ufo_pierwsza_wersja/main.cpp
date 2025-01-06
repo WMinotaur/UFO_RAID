557d7bccc5ec1e27d1a90c71349e01d0d1804b1b
Wojciech <wojciechminior@gmail.com>
2025-01-06 20:57:06 +0100

    Second version and junk removal

--- Ufo_pierwsza_wersja/main.cpp
+++ Ufo_pierwsza_wersja/main.cpp
@@ -5,80 +5,84 @@
 #include <ctime>
 #include "constants.h"
 #include <iostream>
+#include <filesystem>
 
 using namespace sf;
 
-class Background {
-private:
+
+class GameElement {
+protected:
     Texture texture;
     Sprite sprite;
+    bool is_initialised{ false };
+    
 public:
-    bool prepare_enviroment()
-    {
-        texture.loadFromFile("background.jpg");
-        sprite.setTexture(texture);
-        return true;
-    }
-    Background() {
+    
+
+    GameElement (){
         
+     }
+    bool if_initialised() {
+        return is_initialised;
+    }
+    void draw(RenderWindow* okno) {
+        okno->draw(sprite);
+    }
+    void Create(std::string image_path) {
+        is_initialised = texture.loadFromFile(image_path);
+        if (is_initialised == true) {
+            sprite.setTexture(texture);
+        }
+        std::filesystem::path xxx = std::filesystem::current_path();
+    }
+    
+};
+class Background:public GameElement {
+private:
+    std::string path = ".\\Textures\\background.jpg" ;
+
+ public:
+
+     Background():GameElement() {
+         Create(path);
     }
     ~Background()
     {
         std::cout << "destruktor sie wykonal" << std::endl;
     }
 
-    Sprite& getSprite() {
-        return sprite;
-    }
-
 
 };
 
-class Ship {
+class Ship:public GameElement {
+private:
+    std::string path{ ".\\Textures\\ufo.png" };
+    float pozycja_x{};
+    float pozycja_y{};
+     
 public:
-    Ship() {
-        is_initialised = false;
-        
-        if (!texture.loadFromFile("ufo.png")) {
-            is_initialised = false;
-        }
-        else
-        {
-            is_initialised = true;
-        }
-           
-       
-        sprite.setTexture(texture);
+    Ship():GameElement() {
+        Create(path);
         sprite.setScale(0.15f, 0.15f);
-        float x = (constants::window_width - sprite.getGlobalBounds().width) / 2;
-        float y = constants::window_height - sprite.getGlobalBounds().height;
-        sprite.setPosition(x, y);
+        pozycja_x = (constants::window_width - sprite.getGlobalBounds().width) / 2;
+        pozycja_y = constants::window_height - sprite.getGlobalBounds().height;
+        sprite.setPosition(pozycja_x, pozycja_y);
     }
 
     void move(float offsetX, float offsetY) {
+        pozycja_x += offsetX;
+        pozycja_y += offsetY;
+        if (pozycja_x < 0) {
+            pozycja_x = 0;
+            offsetX = 0;
+        }
+        if (pozycja_x + sprite.getGlobalBounds().width > constants::window_width) {
+            pozycja_x = constants::window_width - sprite.getGlobalBounds().width;
+            offsetY = 0;
+        }
         sprite.move(offsetX, offsetY);
-
-        // Ograniczenie ruchu do obszaru okna
-        if (sprite.getPosition().x < 0) {
-            sprite.setPosition(0, sprite.getPosition().y);
-        }
-        if (sprite.getPosition().x + sprite.getGlobalBounds().width > constants::window_width) {
-            sprite.setPosition(constants::window_width - sprite.getGlobalBounds().width, sprite.getPosition().y);
-        }
+        
     }
-
-    Sprite& getSprite() {
-        return sprite;
-    }
-    bool Got_initialized()
-    {
-        return is_initialised;
-    }
-
-private:
-    Texture texture;
-    Sprite sprite;
-    bool is_initialised;
 };
 class Message_box {
 public:
@@ -91,23 +95,24 @@ public:
 
 int main() {
     srand(static_cast<unsigned>(time(NULL)));
+    std::string path{ "C:\\git\\jpo_ufo_gra" };//zmienic na koniec
 
+    std::filesystem::current_path(path);
+    std::filesystem::path xxx = std::filesystem::current_path();
     RenderWindow okno({ constants::window_width, constants::window_height }, "gra_ufo");
     okno.setFramerateLimit(60);
     Background background;
-    if (background.prepare_enviroment() == false)
+    if (background.if_initialised() == false)
     {
         Message_box box("nie udalo sie zaladowac tla", "blad");
         return -1;
     }
     
     Ship ship;
-    if (ship.Got_initialized() == false) {
+    if (ship.if_initialised() == false) {
         Message_box box("nie udalo sie zaladowac statku", "blad");
         return -1;
     }
-    
-
     while (okno.isOpen()) {
         Event event;
         while (okno.pollEvent(event)) {
@@ -129,8 +134,8 @@ int main() {
 
         // Rysowanie
         okno.clear();
-        okno.draw(background.getSprite());
-        okno.draw(ship.getSprite());
+        background.draw(&okno);
+        ship.draw(&okno);
         okno.display();
     }
 
